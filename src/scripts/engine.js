7e0c539a7239506b69c9635d4f6936a589a7b277
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-12-04 08:36:41 -0300

    Adicionando eventos de keydown

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -8,6 +8,10 @@ const playTune = (key) => {
 }
 
 pianoKeys.forEach((key) => {
-    console.log(key.dataset.key)
+    // console.log(key.dataset.key)
     key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+document.addEventListener("keydown", (e) => {
+    playTune(e.key);
 });
